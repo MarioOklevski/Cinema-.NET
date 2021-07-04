697d589b2a6afb8386b151464230ff35614549e8
Fanta16 <oklevskimario@gmail.com>
2021-07-04 11:42:30 +0200

    Custom User and Models

---
+++ CinemaApp/Cinema.Web/Models/Domain/MovieInShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema.Web.Models.Domain
+{
+    public class MovieInShoppingCart
+    {
+        public Guid MovieId { get; set; }
+        public Guid ShoppingCartId { get; set; }
+        public Movie Movie { get; set; }
+        public ShoppingCart ShoppingCart { get; set; }
+    }
+}
